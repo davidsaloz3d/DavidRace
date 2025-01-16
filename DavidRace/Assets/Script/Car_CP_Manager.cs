a5aabb457525bac5ec09aa3fbb2d9b2738e5fa75
davidsaloz3d <davidsaloz3d@gmail.com>
2025-01-16 13:26:22 +0100

    Posicion defectuoso

--- DavidRace/Assets/Script/Car_CP_Manager.cs
+++ DavidRace/Assets/Script/Car_CP_Manager.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Car_CP_Manager : MonoBehaviour
 {
+    public int cpCrossed = 0;
+    public int NumeroCoche;
+
+    public int CarPosition;
+
+    public RaceManager raceManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +18,12 @@ public class Car_CP_Manager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("CP")){
+            cpCrossed += 1;
+            raceManager.CocheCollectCP(NumeroCoche,cpCrossed);
+        }
     }
 }
--- DavidRace/Assets/Script/RaceManager.cs
+++ DavidRace/Assets/Script/RaceManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RaceManager : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class RaceManager : MonoBehaviour
 
     private int CochesTotales;
     private int ChekpointTotales;
+
+    public Text PosicionTxt;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,6 +22,7 @@ public class RaceManager : MonoBehaviour
         ChekpointTotales = CpHolder.transform.childCount;
 
         SetCheckpoint();
+        setCarPostion();
     }
 
     void SetCheckpoint(){
@@ -35,6 +39,52 @@ public class RaceManager : MonoBehaviour
             CpPaCadaCoche[i].name = "CP " + i;
             CpPaCadaCoche[i].layer = 6 + i;
         }
+    }
+
+    void setCarPostion(){
+        for(int i = 0; i < CochesTotales; i++){
+            Coches[i].GetComponent<Car_CP_Manager>().CarPosition = i + 1;
+            Coches[i].GetComponent<Car_CP_Manager>().NumeroCoche = i;
+        }
+
+        PosicionTxt.text = "POS: " + Coches[0].GetComponent<Car_CP_Manager>().CarPosition + "/" + CochesTotales;
+    }
+
+    public void CocheCollectCP(int carNumber, int CpNumber){
+        CpPaCadaCoche[carNumber].transform.position = CpPosition[CpNumber].transform.position;
+        CpPaCadaCoche[carNumber].transform.rotation = CpPosition[CpNumber].transform.rotation;
+
+        comparePosition(carNumber);
+    }
+
+    void comparePosition(int carNumber){
+        if(Coches[carNumber].GetComponent<Car_CP_Manager>().CarPosition > 1){
+            GameObject currentCar = Coches[carNumber];
+            int currentCarPosition = currentCar.GetComponent<Car_CP_Manager>().CarPosition;
+            int currentCarCP = currentCar.GetComponent<Car_CP_Manager>().cpCrossed;
+
+            GameObject carInFront = null;
+            int carInFrontPos = 0;
+            int carInFrontCP = 0;
+
+            for(int i = 0; i < CochesTotales; i++){
+                if(Coches[i].GetComponent<Car_CP_Manager>().CarPosition == currentCarPosition-1){
+                    carInFront = Coches[i];
+                    carInFrontCP = carInFront.GetComponent<Car_CP_Manager>().cpCrossed;
+                    carInFrontPos = carInFront.GetComponent<Car_CP_Manager>().CarPosition;
+                    break;
+                }
+            }
+
+            if(currentCarCP > carInFrontCP){
+                currentCar.GetComponent<Car_CP_Manager>().CarPosition = currentCarPosition - 1;
+                carInFront.GetComponent<Car_CP_Manager>().CarPosition = carInFrontPos + 1;
+            }
+
+        PosicionTxt.text = "POS: " + Coches[0].GetComponent<Car_CP_Manager>().CarPosition + "/" + CochesTotales;
+
+        }
+
     }
     // Update is called once per frame
     void Update()
